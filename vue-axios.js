cb484e0820e50d443a7b9e5c025833607fd46485
kaizhengya <76873400+kaizhengya@users.noreply.github.com>
2021-03-19 16:19:06 +0800

    Add files via upload

---
+++ vue-axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+export function request(config){ //配置axios
+	const $http = axios.create({
+		timeout:5000
+	})
+	return $http(config)
+}
+
+
+// 新建一个文件 连接接口
+
+
+import {request} from './uits.js'
+export function gethome(data) {
+	return request({
+		url: '/foo',
+		params: data
+	})
+}
+export function getmode(){
+	return request({
+		url:'./cont.js'
+	})
+}
